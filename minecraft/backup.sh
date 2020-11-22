2085880e56d20de60cadfa07dfb642601766f16c
Jamie Quigley <jamie@quigley.xyz>
2020-11-22 12:42:01 +0000

    Added prune command in backup.sh

--- minecraft/backup.sh
+++ minecraft/backup.sh
@@ -15,3 +15,5 @@ sleep 2
 borg create -C lzma "$BACKUP_DIR::$SERVER_NAME-{now}" .
 rcon.py save-on
 rcon.py tellraw @a '["",{"text":"["},{"text":"Server","color":"dark_red"},{"text":"]: World backup finished"}]'
+echo Pruning backups older than 1 week
+borg prune --keep-within 1w  --keep-daily 365 "$BACKUP_DIR"
